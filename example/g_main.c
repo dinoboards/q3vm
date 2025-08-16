aefb8d43df8b4b40bf1c3d81373e4be02836e960
Dean Netherton <dinoboards@netherton.org>
2025-08-16 16:34:35 +1000

    vm: restore tests - identified broken float tests

--- example/g_main.c
+++ example/g_main.c
@@ -15,6 +15,9 @@ This must be the very first function compiled into the .qvm file
 ================
 */
 
+char d;
+int  nn = 0;
+
 typedef unsigned char uint8_t;
 typedef char          int8_t;
 
@@ -24,27 +27,62 @@ typedef short          int16_t;
 typedef unsigned long uint32_t;
 typedef long          int32_t;
 
-float        f;
-float        r;
-unsigned int xx;
-
-float alpha(float x);
+uint8_t xyz = 0;
 
 // these are int32_t
 int vmMain(int command, int arg0, int arg1, int arg2) {
 
-  f  = 1.2;
-  xx = 99;
+  char     str[] = "local string ref\n";
+  uint8_t  ua;
+  int8_t   a;
+  uint16_t ub;
+  int16_t  b;
+  uint32_t ud;
+  int32_t  d;
 
-  f = f + xx;
+  float f;
 
-  printf("f: %f\n", f);
-}
+  xyz = 34;
 
-float alpha(float f) {
+  a  = -123;
+  ub = 45000;
+  b  = -32000;
+  ud = 0x12345678;
+  d  = 0x11223344;
 
-  int xx = 99;
-  return f + xx;
+  f = 1.345;
+
+  switch (command) {
+  case 0:
+    printf("ua: %d, a: %d, ub: %d, b: %d, ud: %d, d:%d, f:%f\r\n", ua, a, ub, b, ud, d, f);
+    printf(str);
+    printf("!!sizeof(int): %d\n", sizeof(int));
+    printf("Hello World! - fib(5) = %i\n", fib(5));
+    printf("someimmutabledata at: %d\n", (int)&someimmutabledata);
+    printf("changingdata at: %d\n", (int)&changingdata);
+    printf("nn(%d) = %d\n", &nn, nn);
+    return 0;
+
+  case 1:
+    printf("Command 1\n");
+    return 0;
+
+  case 2:
+    d = *((int *)&vmMain);
+    return 0;
+
+  case 3:
+    d = someimmutabledata[1];
+    return 0;
+
+  case 4:
+    d = changingdata[1];
+    return 0;
+
+  default:
+    printf("Unknown command.\n");
+    return -1;
+  }
 }
 
 void printf(const char *fmt, ...) {
@@ -59,6 +97,7 @@ void printf(const char *fmt, ...) {
 }
 
 int fib(int n) {
+  nn = n;
   if (n <= 2)
     return 1;
   else
