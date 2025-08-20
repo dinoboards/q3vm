e5706383828b2f10e60d25e23af36e47b375a2e9
Dean Netherton <dinoboards@netherton.org>
2025-08-20 19:48:53 +1000

    vm: all verification tests passing

--- verification/chapter_11/valid/long_expressions/assign.c
+++ verification/chapter_11/valid/long_expressions/assign.c
@@ -2,12 +2,12 @@ int main(void) {
   /* initializing a tests the rewrite rule for
    * movq $large_const, memory_address
    */
-  long a = 4294967290l;
+  long a = 16777210L;
   long b = 0l;
   /* Assign the value of one long variable
    * (which is too large for an int to represent)
    * to another long variable
    */
   b = a;
-  return (b == 4294967290l);
+  return (b == 16777210L);
 }
--- verification/chapter_11/valid/long_expressions/static_long.c
+++ verification/chapter_11/valid/long_expressions/static_long.c
@@ -1,11 +1,11 @@
 /* Test initializing and updating a long global variable */
-static long foo = 4294967290l;
+static long foo = 16777210l;
 
 int main(void) {
-  if (foo + 5l == 4294967295l) {
-    // assign a constant that can't fit in 32 bits; tests assembly rewrite rule
-    foo = 1152921504606846988l;
-    if (foo == 1152921504606846988l)
+  if (foo + 5l == 16777215l) {
+    // assign a constant that can't fit in 24 bits; tests assembly rewrite rule
+    foo = 268435468L;
+    if (foo == 268435468L)
       return 1;
   }
   return 0;
