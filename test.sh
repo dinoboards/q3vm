a540a8b868814e2758cc76e47fff4f7f20bb3ccf
Dean Netherton <dinoboards@netherton.org>
2025-08-25 12:48:45 +1000

    vm: change programCounter from index from codeBase to pointer to code base

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ set +e
 # make clean && make test
 make clean && make test && (cd ./verification && ./test.sh) && (cd ./example && ../q3vm bytecode.qvm; echo $?)
 # make clean && make && (cd ./verification && ./test.sh) && (cd ./example && ../q3vm bytecode.qvm; echo $?)
-make clean && make test && (cd ./verification && ./test.sh ez80) && (cd ./example && ../q3vm bytecode.qvm; echo $?)
+# make clean && make test && (cd ./verification && ./test.sh ez80) && (cd ./example && ../q3vm bytecode.qvm; echo $?)
 # make clean && make && (cd ./example && make && ../q3vm bytecode.qvm; echo $?)
 #
